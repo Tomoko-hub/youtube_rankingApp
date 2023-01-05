b00e016df921e58cead5545b4dedaa96164689c2
Tomoko-hub <haisaitomoco@gmail.com>
2023-01-05 21:11:44 +0200

    Add project files.

---
+++ youtube_rankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,27 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const RankItems = () => {
+    
+    const [items, setItmes] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return resuls.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    }, []);
+
+    return (
+        ¨<main>
+            {
+                (items != null) ? items.map(() => <h3>{ item.title }</h3>) : <div>Loading...</div>
+             }
+        </main>
+        )
+}
+
+export default RankItems;
